cdd6fd1c1a3df77e3cba836213ef0a891aeab01c
Mostafa <mostafa.rafaie@gmail.com>
2018-02-26 15:25:12 -0600

    Update Genom Structure __init__

--- genom_struct.py
+++ genom_struct.py
@@ -1,12 +1,13 @@
-"""
-__author__ = "Mostafa Rafaie"]
+import os
+import sys
+
+__author__ = "Mostafa Rafaie"
 __license__ = "APLv2"
-"""
 
 
 class ChromosomesStruct:
-    def __init(self, name, min_value, max_value, floating_point,
-               is_fixed=False):
+    def __init__(self, name, min_value, max_value, floating_point,
+                 is_fixed=False):
         self.name = name
         self.min_value = min_value
         self.max_value = max_value
@@ -15,5 +16,18 @@ class ChromosomesStruct:
 
 
 class GenomStruct:
-    def __init(self, ):
-        pass
+    def __init__(self, path):
+        if os.path.isfile(path) is not True:
+            print ('The Genom File Structure "{}" is not avaliable'.
+                   format(path))
+            sys.exit(1)
+
+        self.chromosomes_structs = []
+
+        with open(path, 'r') as fi:
+            lines = fi.readline()
+            for i in range(len(lines)):
+                l = lines[i].split(',')
+                self.chromosomes_structs[i] = ChromosomesStruct(l[0], l[1],
+                                                                l[2], l[3],
+                                                                l[4])
