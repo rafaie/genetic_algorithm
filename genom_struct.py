4abcb8b57dcc88b76bcfbcdf863cfb7a0ef8eb6f
Mostafa <mostafa.rafaie@gmail.com>
2018-02-26 15:13:23 -0600

    add genom structure file

---
+++ genom_struct.py
@@ -0,0 +1,19 @@
+"""
+__author__ = "Mostafa Rafaie"]
+__license__ = "APLv2"
+"""
+
+
+class ChromosomesStruct:
+    def __init(self, name, min_value, max_value, floating_point,
+               is_fixed=False):
+        self.name = name
+        self.min_value = min_value
+        self.max_value = max_value
+        self.floating_point = floating_point
+        self.is_fixed = is_fixed
+
+
+class GenomStruct:
+    def __init(self, ):
+        pass
