1d2febf67183610c434685159951cb9a1f366694
Mostafa <mostafa.rafaie@gmail.com>
2018-02-26 16:09:18 -0600

    Update the sample genom structure

---
+++ test_genom_struct.py
@@ -0,0 +1,27 @@
+"""
+test_genom_struct.py: to test genom structure class.
+
+"""
+
+import unittest
+from genom_struct import GenomStruct
+
+
+__author__ = "Mostafa Rafaie"
+__license__ = "APLv2"
+
+
+class GenomStructTest(unittest.TestCase):
+
+    def test_genom_struct(self):
+        path = 'sample_genom_struct.csv'
+
+        gs = GenomStruct(path)
+
+        print(gs.cs[5])
+        
+        self.assertEqual(len(gs.cs), 8)
+        self.assertAlmostEqual(gs.cs[3].name, 'q4')
+        self.assertAlmostEqual(gs.cs[3].min_value, 1)
+        self.assertAlmostEqual(gs.cs[3].max_value, 8)
+        self.assertAlmostEqual(gs.cs[5].is_fixed, False)
