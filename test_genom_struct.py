53b0e2ed6faa5f8ac5e746772e58e96d68121208
Mostafa <mostafa.rafaie@gmail.com>
2018-02-27 12:15:29 -0600

    add test for random_genom

--- test_genom_struct.py
+++ test_genom_struct.py
@@ -18,7 +18,6 @@ class GenomStructTest(unittest.TestCase):
 
         gs = GenomStruct(path)
 
-        # print(gs.cs[5])
         self.assertEqual(len(gs.cs), 8)
         self.assertAlmostEqual(gs.cs[3].name, 'q4')
         self.assertAlmostEqual(gs.cs[3].min_value, 1)
@@ -28,3 +27,8 @@ class GenomStructTest(unittest.TestCase):
         r = gs.rand(2)
         self.assertGreaterEqual(r, gs.cs[2].min_value)
         self.assertLessEqual(r, gs.cs[2].max_value)
+
+        genom = gs.random_genom()
+        self.assertGreaterEqual(len(genom), len(gs.cs))
+        self.assertGreaterEqual(genom[2], gs.cs[2].min_value)
+        self.assertLessEqual(genom[4], gs.cs[4].max_value)
