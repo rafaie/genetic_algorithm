5e71b4fb9de3ae53a8da27c13348aa5fd4ed96ad
Mostafa <mostafa.rafaie@gmail.com>
2018-03-04 10:45:28 -0600

    Update fitness function

--- ga_function_optimization.py
+++ ga_function_optimization.py
@@ -19,8 +19,8 @@ B = 1
 
 # Using inverse function for fitness 1/F(x)
 def calc_fitness(genom):
-    return 1/abs(sum([v * genom[i] for i, v in
-                 enumerate(FUNCTION)]) + B)
+    return abs(sum([v * genom[i] for i, v in
+               enumerate(FUNCTION)]) + B)
 
 
 if __name__ == "__main__":
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     init_population_size = 10000
     population_size = 100
     mutation_rate = 0.15
-    num_iteratitions = 100
+    num_iteratitions = 1000
     crossover_type = GeneticAlgorithm.TWO_POINT_CROSSOVER
     fitness_goal = 0.00001
 
@@ -39,7 +39,8 @@ if __name__ == "__main__":
     population = ga.run(init_population_size, population_size,
                         mutation_rate, num_iteratitions, crossover_type,
                         calc_fitness, fitness_goal,
-                        cuncurrency=1)
+                        cuncurrency=1,
+                        reverse_fitness_order=False)
 
     print(population[:3].astype(float))
     print(population[:, -1].astype(float))
