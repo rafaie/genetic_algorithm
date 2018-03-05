b871f283e924a0f37a8090fa5cd27f9198ebe614
Mostafa <mostafa.rafaie@gmail.com>
2018-03-05 00:41:29 -0600

    Extract Runtime and update the parameteres!

--- ga_function_optimization.py
+++ ga_function_optimization.py
@@ -6,6 +6,7 @@ Function: F(x) = 12a + 24b -35c +7e -5c + 1
 
 import logging.config
 import yaml
+import time
 from genetic_algorithm import GeneticAlgorithm
 
 __author__ = "Mostafa Rafaie"
@@ -28,19 +29,26 @@ if __name__ == "__main__":
 
     path = 'sample_genom_struct.csv'
     init_population_size = 10000
-    population_size = 100
+    population_size = 200
     mutation_rate = 0.15
-    num_iteratitions = 1000
+    num_iteratitions = 400
     crossover_type = GeneticAlgorithm.TWO_POINT_CROSSOVER
     fitness_goal = 0.00001
 
     ga = GeneticAlgorithm(path)
 
+    start_time = time.time()
     population = ga.run(init_population_size, population_size,
                         mutation_rate, num_iteratitions, crossover_type,
                         calc_fitness, fitness_goal,
                         cuncurrency=1,
                         reverse_fitness_order=False)
-
+    end_time = time.time()
     print(population[:3].astype(float))
     print(population[:, -1].astype(float))
+    print('Runtime :', end_time - start_time)
+
+    # Runtime : 49.50081205368042, cuncurrency = 20
+    # Runtime : 34.471755027770996, cuncurrency = 10
+    # Runtime : 26.63480496406555, cuncurrency = 4
+    # Runtime : 20.886075973510742, cuncurrency = 1
